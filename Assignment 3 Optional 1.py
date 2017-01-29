22f05f17a155e278572b9d4dbd71fd91c04ea305
Ritterbruder <ritterbruder.by@gmail.com>
2017-01-29 21:14:23 +0300

    Create Assignment 3 Optional 1.py

---
+++ Assignment 3 Optional 1.py
@@ -0,0 +1,8 @@
+def plot9():
+    import matplotlib as plt
+    %matplotlib inline
+    
+    Top15 = answer_one()
+    Top15['PopEst'] = Top15['Energy Supply'] / Top15['Energy Supply per Capita']
+    Top15['Citable docs per Capita'] = Top15['Citable documents'] / Top15['PopEst']
+    Top15.plot(x='Citable docs per Capita', y='Energy Supply per Capita', kind='scatter', xlim=[0, 0.0006])
